c62fd7ca8e32cd4652546189e44d06a68528728a
arlo <aroos2147@gmail.com>
2025-08-03 08:25:24 -0700

    GameEnvironment statically accessible constants

---
+++ src/Common/Singleton.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace grapple_gods;
+
+public abstract class Singleton<T>
+    where T : Singleton<T>
+{
+    private static readonly Lazy<T> Lazy =
+        new(() => (Activator.CreateInstance(typeof(T), true) as T)!);
+
+    public static T Instance => Lazy.Value;
+}
