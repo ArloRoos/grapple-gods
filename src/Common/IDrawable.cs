254d8ee2f9012d8431e3fe358839b2da1616716b
arlo <aroos2147@gmail.com>
2025-08-03 07:36:06 -0700

    Added drawable and updateable interfaces

---
+++ src/Common/IDrawable.cs
@@ -0,0 +1,9 @@
+namespace grapple_gods;
+
+public interface IDrawable
+{
+    /// <summary>
+    /// Draws this object to the screen.
+    /// </summary>
+    void Draw();
+}
---
+++ src/Common/IUpdateable.cs
@@ -0,0 +1,9 @@
+namespace grapple_gods;
+
+public interface IUpdateable
+{
+    /// <summary>
+    /// Updates this object in the current game frame.
+    /// </summary>
+    void Update();
+}
